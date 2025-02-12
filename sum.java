9aba37afbfe73637250bca781a24c2127b4ea039
GOPINATH <ngopi6769@gmail.com>
2025-02-12 22:32:46 +0530

    commit

---
+++ sum.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class sum {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+
+        //get input from user
+        System.out.println("enter the element in an array :");
+        int [] arr=new int[3];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        //main logic
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum=sum+arr[i];
+        }
+
+        System.out.println("sum of the array element is "+sum);
+
+
+    }
+    
+}

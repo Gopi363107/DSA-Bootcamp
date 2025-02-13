eee90ff9f7b47d6b0d84204bbc0f6a4c8ec7dd8a
GOPINATH <ngopi6769@gmail.com>
2025-02-13 13:24:04 +0530

    commit

---
+++ secsmallest.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class secsmallest {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+
+        //get n input from the user
+        System.out.println("enter the value of n :");
+        int n=sc.nextInt();
+
+        //get array element values
+        System.out.println("enter the element for an array :");
+        int [] arr=new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        //print the original array
+        System.out.println("your original array :");
+        System.out.println(Arrays.toString(arr));
+
+        //main logic
+        int min=arr[0];
+        int smin=Integer.MIN_VALUE;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]<min){
+                smin=min;
+                min=arr[i];
+            }
+            else if( arr[i] > min && arr[i] < smin){
+                smin=arr[i];
+            }
+        }
+    }
+    
+}

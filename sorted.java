12a80c53f3b94f11b4aa8c80e01153033038dfb7
GOPINATH <ngopi6769@gmail.com>
2025-02-14 17:03:37 +0530

    commit

---
+++ sorted.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class sorted {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int [] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(SortedOrNot(arr));
+    }    
+
+    static boolean SortedOrNot(int arr[]){
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>arr[i-1]){
+
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+
+
+    }
+        
+    
+}
